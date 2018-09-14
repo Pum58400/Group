cd94bf880557958763cef48763ee0da7899b5f13
Pum58400 <giggleinheart@hotmail.com>
2018-09-14 23:01:37 +0700

    Gui no Data

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        Register RegisterPanel = new Register();
+        frame.setContentPane(RegisterPanel.getMainPanel());
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
---
+++ src/Register.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+
+public class Register {
+    private JTextField txtName;
+    private JTextField txtUserName;
+    private JPasswordField pwdPassword;
+    private JPasswordField pwdConPassword;
+    private JButton btnOk;
+    private JButton btnCancel;
+    private JLabel lblImg;
+    private JLabel lblName;
+    private JLabel lblUserName;
+    private JLabel lblPassword;
+    private JLabel lblConPassword;
+    private JPanel mainPanel;
+
+
+
+    public JPanel getMainPanel(){
+        return mainPanel;
+
+
+   }
+}
